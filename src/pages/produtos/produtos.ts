6d6f242222cae7cb7cc96904b32a7834f6436b46
Edney Siqueira <edneyfs@gmail.com>
2020-09-11 16:03:45 -0300

    Carregando imagens dos produtos

--- src/pages/produtos/produtos.ts
+++ src/pages/produtos/produtos.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { ProdutoDTO } from '../../models/produto.dto';
 import { ProdutoService } from '../../services/domain/produto.service';
+import { API_CONFIG } from '../../config/api.config';
 
 @IonicPage()
 @Component({
@@ -15,20 +16,34 @@ export class ProdutosPage {
   constructor(
       public navCtrl: NavController, 
       public navParams: NavParams,
-      public produtoServoce: ProdutoService) {
+      public produtoService: ProdutoService) {
   }
 
   ionViewDidLoad() {
     //pegando o pametro passado na navegacao
     let categoria_id = this.navParams.get("categoria_id");
-    this.produtoServoce.findByCategoria(categoria_id).subscribe(
+    this.produtoService.findByCategoria(categoria_id).subscribe(
         resposta => {
             //é uma pesquisa paginada, retorna um Page - JSON: { "content": [ ...
             this.items = resposta["content"];
+            this.loadImageUrls();
         },
         error => {}
     );
+  }
 
+  loadImageUrls() {
+    for (let index = 0; index < this.items.length; index++) {
+      let item = this.items[index];
+      // faz o getImageFromBucket apenas para ir no servidor e ver se tem a image, caso não tenha, imageUrl fica nullo e eh mostrado imagem default na tela. 
+      this.produtoService.getSmallImageFromBucket(item.id)
+        .subscribe(
+            resposta => {
+                item.imageUrl = `${API_CONFIG.bucketBaseUrl}/prod${item.id}-small.jpg`;
+            },
+            error => {}
+        );
+    }
   }
 
 }
--- src/services/domain/produto.service.ts
+++ src/services/domain/produto.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 import { Observable } from "rxjs";
-import { ProdutoDTO } from "../../models/produto.dto";
 import { API_CONFIG } from "../../config/api.config";
 
 @Injectable()
@@ -14,5 +13,13 @@ export class ProdutoService {
 
     findByCategoria(categoria_id : string) {
         return this.http.get(`${API_CONFIG.baseUrl}/produtos/?categorias=${categoria_id}`);
+    }
+
+    // any aceita qualquer coisa, coringa
+    getSmallImageFromBucket(id : string) : Observable<any> {
+        let url = `${API_CONFIG.bucketBaseUrl}/prod${id}-small.jpg`;
+        //respota é um blob, não é um json
+        let imagem = this.http.get(url, {responseType : 'blob'});
+        return imagem;
     }
 }
