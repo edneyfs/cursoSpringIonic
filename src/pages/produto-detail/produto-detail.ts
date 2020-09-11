3e58ed3470009fda6f7e3cfeb936d934e25671e7
Edney Siqueira <edneyfs@gmail.com>
2020-09-11 16:25:51 -0300

    Carregando dados do produto

--- src/pages/produto-detail/produto-detail.ts
+++ src/pages/produto-detail/produto-detail.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { ProdutoDTO } from '../../models/produto.dto';
+import { ProdutoService } from '../../services/domain/produto.service';
+import { API_CONFIG } from '../../config/api.config';
 
 @IonicPage()
 @Component({
@@ -11,15 +13,32 @@ export class ProdutoDetailPage {
 
   item : ProdutoDTO;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(
+    public navCtrl: NavController, 
+    public navParams: NavParams,
+    public produtoService: ProdutoService) {
   }
 
   ionViewDidLoad() {
-    this.item = {
-      id: "1",
-      nome: "Mouse",
-      preco: 12.80
-    }
+    let id = this.navParams.get("id");
+    this.produtoService.findById(id).subscribe(
+      resposta => {
+        this.item = resposta;
+        this.getImageIfExists();
+      },
+      erros => {}
+    );
+  }
+
+  getImageIfExists() {
+    // faz o getImageFromBucket apenas para ir no servidor e ver se tem a image, caso não tenha, imageUrl fica nullo e eh mostrado imagem default na tela. 
+    this.produtoService.getImageFromBucket(this.item.id)
+      .subscribe(
+          resposta => {
+              this.item.imageUrl = `${API_CONFIG.bucketBaseUrl}/prod${this.item.id}.jpg`;
+          },
+          error => {}
+      );
   }
 
 }
--- src/services/domain/produto.service.ts
+++ src/services/domain/produto.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 import { Observable } from "rxjs";
 import { API_CONFIG } from "../../config/api.config";
+import { ProdutoDTO } from "../../models/produto.dto";
 
 @Injectable()
 export class ProdutoService {
@@ -11,6 +12,10 @@ export class ProdutoService {
 
     }
 
+    findById(id : string) {
+        return this.http.get<ProdutoDTO>(`${API_CONFIG.baseUrl}/produtos/${id}`);
+    }
+
     findByCategoria(categoria_id : string) {
         return this.http.get(`${API_CONFIG.baseUrl}/produtos/?categorias=${categoria_id}`);
     }
@@ -18,6 +23,13 @@ export class ProdutoService {
     // any aceita qualquer coisa, coringa
     getSmallImageFromBucket(id : string) : Observable<any> {
         let url = `${API_CONFIG.bucketBaseUrl}/prod${id}-small.jpg`;
+        //respota é um blob, não é um json
+        let imagem = this.http.get(url, {responseType : 'blob'});
+        return imagem;
+    }
+
+    getImageFromBucket(id : string) : Observable<any> {
+        let url = `${API_CONFIG.bucketBaseUrl}/prod${id}.jpg`;
         //respota é um blob, não é um json
         let imagem = this.http.get(url, {responseType : 'blob'});
         return imagem;
