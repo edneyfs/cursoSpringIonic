5a8244ad412dd6a3fb0d7c3d4d2ed884f6981117
Edney Siqueira <edneyfs@gmail.com>
2020-09-11 09:41:55 -0300

    Tratamento padrao para outros erros com Alert

--- src/interceptors/error-interceptors.ts
+++ src/interceptors/error-interceptors.ts
@@ -2,11 +2,15 @@ import { Injectable } from '@angular/core';
 import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { Observable } from 'rxjs/Rx'; // IMPORTANTE: IMPORT ATUALIZADO
 import { StorageService } from '../services/storage.service';
+import { AlertController } from 'ionic-angular/components/alert/alert-controller';
+import { Title } from '@angular/platform-browser';
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
 
-    constructor(public storage: StorageService) {
+    constructor(
+        public storage: StorageService,
+        public alertCtrl: AlertController) {
     }
 
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
@@ -27,9 +31,16 @@ export class ErrorInterceptor implements HttpInterceptor {
             console.log(errorObj);
 
             switch(errorObj.status){
+                case 401:
+                    this.handle401();
+                    break;
+
                 case 403:
                     this.handle403();
                     break;
+
+                default:
+                    this.handleDefaultError(errorObj);
             }
 
             //levanta de novo o erro
@@ -39,6 +50,38 @@ export class ErrorInterceptor implements HttpInterceptor {
 
     handle403() {
         this.storage.setLocalUser(null);
+    }
+
+    handle401() {
+        let alert = this.alertCtrl.create({
+            title: 'Erro 401: falha de autenticação',
+            message: 'Email ou senha incorretos',
+            //para o alert sumir apenas quando clicar no botao OK
+            enableBackdropDismiss: false,
+            //definir a lista de botoes
+            buttons: [
+                {
+                    text: 'Ok'
+                }
+            ]
+        });
+        alert.present();
+    }
+
+    handleDefaultError(errorObj ) {
+        let alert = this.alertCtrl.create({
+            title: "Erro " + errorObj.status + ": " +  errorObj.error,
+            message: errorObj.message,
+            //para o alert sumir apenas quando clicar no botao OK
+            enableBackdropDismiss: false,
+            //definir a lista de botoes
+            buttons: [
+                {
+                    text: 'Ok'
+                }
+            ]
+        });
+        alert.present();
     }
 }
 
